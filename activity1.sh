635a92cd47a4612834624513a262d99c9ada4513
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2025-02-05 01:44:48 +0000

    Feb. 5 Class

---
+++ activity1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter your name: "
+read NAME
+
+echo "Enter your age: "
+read AGE
+
+echo "Enter program: "
+read PROGRAM
+
+echo "Hello $NAME. This is your $AGE & your program is $PROGRAM."
---
+++ sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+USER_NAME="Faye"
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+echo "Enter your name: "
+read USER_NAME
+echo "Nice to meet you, $USER_NAME!"
