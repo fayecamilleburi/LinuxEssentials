1291885170065888908cc08cb24e4a42f9cadc17
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2025-04-02 01:18:27 +0000

    Feb. 12 Activity c/o Macorol (Checking)

---
+++ activity3.sh
@@ -0,0 +1,10 @@
+echo "Enter your name: "
+read $name
+
+echo "Enter your Age: "
+read $age
+
+echo "Enter your favorite programming language: "
+read $progLang
+
+echo "Thank you $name, you are $age years old & your favorite programming language is $progLang"
