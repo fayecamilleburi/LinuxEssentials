fc79172850e95482048e8d48d5c58bd2d61cecc5
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2025-04-16 22:53:18 +0800

    update Activity 6

--- log_time.sh
+++ log_time.sh
@@ -1,1 +1,7 @@
-echo "Current Date and Time: $(date)" >> log.txt
+#!/bin/bash
+
+# Get GitHub username from environment (set default if not found)
+GITHUB_USER=${GITHUB_ACTOR:-"unknown_user"}
+
+# Append timestamp and username to log.txt
+echo "$(date) - Logged by $GITHUB_USER" >> log.txt
