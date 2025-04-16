fde0e4c4173ddadf409fc51284a33e8eb410f438
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2025-04-16 23:10:23 +0800

    Delete log_time.sh

--- log_time.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get GitHub username from environment (set default if not found)
-GITHUB_USER=${GITHUB_ACTOR:-"unknown_user"}
-
-# Append timestamp and username to log.txt
-echo "$(date) - Logged by $GITHUB_USER" >> log.txt
